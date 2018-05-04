cea05303226df1d8611cb3665829c95ad217b6d4
jingruoyu <jingruoyu@163.com>
2018-05-04 22:52:25 +0800

    建站首页

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,81 +1,32 @@
 <template>
     <div class="hello">
-        <h1>{{getMsg}}</h1>
-        <p>count: {{count}}</p>
-        <p>countSquare: {{countSquare}}</p>
-        <button @click="increment(3)">addCount</button>
-        <button @click="reset">reset</button>
-        <router-view class="bar"></router-view>
+        <h1 class="welcome">welcome to here</h1>
     </div>
 </template>
 
 <script>
-import {mapState} from 'vuex'
-import {mapGetters} from 'vuex'
-import {mapMutations} from 'vuex'
 
 export default {
     name: 'hello',
     data () {
         return {
-            msg: 'Hello',
-            localCount: 1
         }
     },
     computed: {
-        getMsg () {
-            return this.msg;
-        },
-        /*countSquare () {
-            return this.$store.getters['countModule/countSquare'];
-        },*/
-        ...mapState({
-            //箭头函数,state直接是store中的state
-            count: function (state) {
-                return state.countModule.count;
-            },
-            //与根实例的store挂钩，直接获取this.$store.state.count
-            //countAlias: 'count',
-            //使用局部状态需要常规写法
-            countPlusLocalState (state) {
-                return state.countModule.count + this.localCount
-            }
-        }),
-        ...mapGetters({
-            doneTodosCount: 'todosModule/doneTodosCount',
-            countSquare: 'countModule/countSquare'
-        })
-        
     }, 
     methods: {
-        /*...mapMutations([
-            'countModule.increment',
-            'countModule.reset'
-        ])*/
-        increment (num) {
-            this.$store.commit('countModule/increment',num);
-        },
-        reset () {
-            this.$store.commit('countModule/reset');
-        }
     }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-    font-weight: normal;
-    text-align: left;
-}
-p {
-    text-align: center;
-}
-.hello {
-    padding: 10px;
-    border:1px solid;
-}
-.bar {
-    margin: 10px;
-}
+    .hello {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+        color: white;
+        font-size: 50px;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Hello from '../components/Hello.vue'
-import showId from '../components/showId.vue'
-import bar from '../components/bar.vue'
 import calendar from '../components/price-calendar/calendar.vue'
 import priceCalendar from '../components/price-calendar/price-calendar.vue'
 
@@ -11,35 +9,22 @@ Vue.use(VueRouter)
 const routes =  [
     {
         path: '/',
+        redirect: '/index'
+    },{
+        path: '/index',
         name: 'Hello',
         component: Hello,
-        meta:{title:'hello'},
-        children:[
-        {
-            path:'bar',
-            name:bar,
-            component:bar,
-            meta: { title:'bar' }
-        }
-        ]
-    },
-    {
+        meta:{title:'hello'}
+    },{
         path: '/calendar',
         name: 'calendar',
         meta:{title:'calendar'},
         component: calendar,
-    },
-    {
+    },{
         path: '/priceCalendar',
         name: 'priceCalendar',
         meta:{title:'priceCalendar'},
         component: priceCalendar,
-    },
-    {
-        path: '/:id',
-        name: 'id',
-        meta:{title:'bar'},
-        component: showId,
     }
 ]
 
